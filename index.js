e88c3980987265c1fd0f179f370e637cd613d610
williamandres <williamaguilar@misena.edu.co>
2021-04-09 19:17:03 -0500

    reto 2 task 5 javascript-typescript lección 2

--- index.js
+++ index.js
@@ -1,35 +1,71 @@
 /*
-Para un sistema de gestión de tareas, deseamos registrar
-todas las tareas, cada tarea tiene asociada la siguiente
-información: nombre, fecha de inicio, descripción y estatus,
-el estatus debe ser uno de los siguientes: pendiente, en proceso,
-terminada, cancelada. Además se requiere una lista con todas
-las tareas pendientes, las tareas terminadas y las canceladas.
+Reto 2.
+Queremos implementar un sistema de registro de eventos, para esto, necesitamos guardar una lista con  la información de las personas participantes del evento,  los datos mínimos por participante son: nombre completo, teléfono, correo. Cada evento cuenta con un nombre, precio, fecha y aforo máximo. Se desea guardar una lista con el historial de los eventos y que cada persona pueda ver los datos de los eventos a los que asistió y a los que piensa asistir en un futuro.
 */
-var Tarea = /** @class */ (function () {
-    function Tarea() {
+var Evento = /** @class */ (function () {
+    function Evento() {
     }
-    return Tarea;
+    return Evento;
+}());
+var Participante = /** @class */ (function () {
+    function Participante() {
+    }
+    return Participante;
 }());
 var Estado;
 (function (Estado) {
-    Estado["pendiente"] = "Pendiente";
-    Estado["proceso"] = "En proceso";
-    Estado["terminada"] = "terminada";
-    Estado["cancelada"] = "cancelada";
+    Estado["evento_futuro"] = "evento futuro";
+    Estado["evento_asistido"] = "evento asistido";
 })(Estado || (Estado = {}));
-var tarea = [
+var participantes = [
     {
-        nombreTarea: "Programación",
-        fechaInicioTarea: 25082019,
-        descripcionTarea: " Ejercicos de TypeScript ",
-        estado: Estado.pendiente
+        nombreCompleto: "Hector",
+        telefono: 3133755040,
+        correo: "hectorfabiovasquez@gmail.com",
+        evento: [
+            {
+                nombre: "Concierto",
+                precio: 200,
+                fecha: 8042021,
+                aforoMaximo: 10000,
+                estado: Estado.evento_asistido
+            },
+            {
+                nombre: "ClubLectura",
+                precio: 10,
+                fecha: 4042021,
+                aforoMaximo: 150,
+                estado: Estado.evento_futuro
+            }
+        ]
     },
     {
-        nombreTarea: "ingles",
-        fechaInicioTarea: 25082019,
-        descripcionTarea: " Ejercicos diarios de slang ",
-        estado: Estado.proceso
+        nombreCompleto: "William",
+        telefono: 3100755040,
+        correo: "williamaguilar@gmail.com",
+        evento: [
+            {
+                nombre: "ConciertoRock",
+                precio: 250,
+                fecha: 8042021,
+                aforoMaximo: 5000,
+                estado: Estado.evento_futuro
+            }
+        ]
+    },
+    {
+        nombreCompleto: "Juan",
+        telefono: 3133005040,
+        correo: "juanmedina@gmail.com",
+        evento: [
+            {
+                nombre: "ConciertoSalsa",
+                precio: 280,
+                fecha: 8042021,
+                aforoMaximo: 450,
+                estado: Estado.evento_asistido
+            }
+        ]
     }
 ];
-console.log(tarea);
+console.log(participantes);
